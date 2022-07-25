92ab8a4e7bb8b8aaaf60247f515c266eb7177e6d
nithinneel <105333944+nithinneel@users.noreply.github.com>
2022-07-25 13:14:08 +0530

    Update NithinGitBashPush.java

--- src/test/java/com/NithinGitBash/NithinGitBashPush.java
+++ src/test/java/com/NithinGitBash/NithinGitBashPush.java
@@ -3,5 +3,7 @@ package com.NithinGitBash;
 public class NithinGitBashPush {
 	public static void main(String[] args) {
 		System.out.println("hi this is from git bash");
+		System.out.println("pull to git bash");
+		
 	}
 }
